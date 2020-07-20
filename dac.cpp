82bd13a91e072ae9a465e2f45353da327633009c
nikos chalikias <nikos.chalikias@gmail.com>
2020-07-20 14:45:20 +0100

    use value

--- dac.cpp
+++ dac.cpp
@@ -8,8 +8,8 @@ nBlock_DAC::nBlock_DAC(PinName pinDAC): _dac(pinDAC) {
 void nBlock_DAC::triggerInput(uint32_t inputNumber, uint32_t value) {
     // Input 0 triggers a read regardless of value
     float finput;
-    if (inputNumber < 65535) {
-        finput = (float)inputNumber/65535; //make floating normalized 0-1
+    if (value < 65535) {
+        finput = (float)value/65535; //make floating normalized 0-1
         _dac = finput;                 
     }
 }
